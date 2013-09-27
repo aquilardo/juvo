779bdd58e4f475c816b81ff9189319e420e10419
Alan Torrico <alantorrico@gmail.com>
2013-09-27 13:12:27 -0400

    Conexion a la Base de Dstos

---
+++ model/constantes.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ *Definicion de constantes para la conexion a la base de datos 
+ */
+
+define("DB_SERVER", "localhost");
+define("DB_PORT", "5432");
+define("DB_USER", "postgres");
+define("DB_PASS", "admin");
+define("DB_NAME", "juezdb");
+
+?>
