6966d01473169bc84f4876417a3819f4c7194fe6
Alan Torrico <alantorrico@gmail.com>
2013-09-27 13:41:47 -0400

    conexion sucessfull

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    require './model/conexion.php';
+    
+    $db = new conexion();
+    
+    $consulta = "
+        SELECT *
+        FROM entrenador";
+    
+    $entrenadores = $db->consultar($consulta);
+    
+    while($entrenador = pg_fetch_object($entrenadores)){
+        echo $entrenador->nombre_entrenador.'<br>';
+    }
+?>
